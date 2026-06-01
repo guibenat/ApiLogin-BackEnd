6ba1603ff1e9f04b1865d3ef0f2361da58528cad
guibenat <guibenattedev@gmail.com>
2026-05-31 22:43:48 -0300

    Feat: create controllers and dto

---
+++ login-auth-api/src/main/java/com/example/login_auth_api/contorllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.login_auth_api.contorllers;
+
+import com.example.login_auth_api.dto.LoginRequestDto;
+import com.example.login_auth_api.infra.security.TokenService;
+import com.example.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDto body) {
+
+    }
+}
